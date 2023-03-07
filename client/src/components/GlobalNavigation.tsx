7f9eff8650e09ae45fb858423fca252b830eb47c
yunho <kimyouknow@naver.com>
2023-03-07 15:48:32 +0900

    Refactor: routerInfo filter 유틸

--- client/src/components/GlobalNavigation.tsx
+++ client/src/components/GlobalNavigation.tsx
@@ -1,19 +1,22 @@
 import { NavLink } from 'react-router-dom'
 
-import { GNB_LINK } from '@/constants/route.constant'
+import { gnbLinks } from '@/router/routerInfo'
 
 interface GlobalNavigationProps {
   className?: string
 }
 
+const isLogin = () => true
+
 const GlobalNavigation = ({ className }: GlobalNavigationProps) => {
+  const links = gnbLinks(isLogin())
   return (
     <nav className={className}>
       <ul className="flex flex-col gap-4">
-        {GNB_LINK.map(({ title, link }) => (
-          <li className="w-24 font-semibold text-gray-800 hover:text-blue-500">
-            <NavLink to={link} className={({ isActive }) => (isActive ? 'text-blue-500' : '')}>
-              {title}
+        {links.map(({ label, path }) => (
+          <li key={path} className="w-24 font-semibold text-gray-800 hover:text-blue-500">
+            <NavLink to={path} className={({ isActive }) => (isActive ? 'text-blue-500' : '')}>
+              {label}
             </NavLink>
           </li>
         ))}
