4f8758507f41e5b66c90f7791af3ef12409e90ac
yunho <kimyouknow@naver.com>
2023-03-14 18:01:54 +0900

    Feat: Create User Model

--- server/src/model/User.ts
+++ server/src/model/User.ts
@@ -1,9 +1,21 @@
-const userModel = {
-  create: () => {
-    console.log('create')
-  },
-  update: () => {
-    console.log('update')
-  },
+import mongoose, { Schema } from 'mongoose'
+
+export interface IUser {
+  id: string
+  nickname: string
 }
-export default userModel
+
+const userSchema = new Schema<IUser>({
+  id: {
+    type: String,
+    required: true,
+  },
+  nickname: {
+    type: String,
+    required: true,
+  },
+})
+
+const UserModel = mongoose.model('User', userSchema)
+
+export default UserModel
