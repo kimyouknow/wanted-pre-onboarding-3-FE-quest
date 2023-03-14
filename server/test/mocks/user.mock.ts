ab1ab35ef9bb3d3b51c2361defe01610ae95dbb5
yunho <kimyouknow@naver.com>
2023-03-14 18:03:11 +0900

    Feat: POST /api/auth/signup
    - unit test
    - integration test
    - controller

--- server/test/mocks/user.mock.ts
+++ server/test/mocks/user.mock.ts
@@ -2,9 +2,7 @@ import { faker } from '@faker-js/faker/locale/ko'
 
 export const createRandomUserInfo = () => ({
   id: faker.datatype.uuid(),
-  email: faker.internet.email(),
-  name: faker.name.fullName(),
-  image: faker.image.avatar(),
+  nickname: faker.name.fullName(),
 })
 
 export const createRandomUserList = (length: number) => Array.from({ length }, () => createRandomUserInfo())
